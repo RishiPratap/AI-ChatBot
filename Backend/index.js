13abb5a8f6e061e1901288e69c7d2cdc4405d288
Srihari S <sshsrn@gmail.com>
2023-02-27 19:31:22 +0530

    feat: set up openai
    Signed-off-by: Srihari S <sshsrn@gmail.com>

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,33 @@
 const http = require("http");
 var express = require("express");
 var bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 const cors = require("cors");
+const { Configuration, OpenAIApi } = require("openai");
+
 const port = 3000;
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const getRes = async (openai, userPrompt) => {
+  let res = await openai.createCompletion({
+    model: "text-curie-001",
+    // Find the other models here: https://platform.openai.com/playground/
+    prompt: userPrompt,
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  console.log(res.data.choices[0].text);
+  return res.data.choices[0].text;
+};
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,7 +35,13 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send("Nothing to see here😌");
+});
+
+app.post("/ask", async (req, res) => {
+  const userPrompt = req.body.prompt;
+  const response = await getRes(openai, userPrompt);
+  res.send(response);
 });
 
 app.listen(port);
