b32446eac4db71a46c70331e99e2047def1fbe23
Rishi Pratap <rp0258@srmist.edu.in>
2023-03-09 01:13:52 +0530

    add recaptcha

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import {auth, provider,signInWithPopup} from './firebase';
+import ReCaptcha from 'react-google-recaptcha';
+import { useRef } from 'react';
+import axios from 'axios';
 import './App.css'
   
 const Login = () => {
+
+    const captchaRef = useRef(null)
   
     // Sign in with google
-    const signin = () => {
+    const signin = async() => {
+        const token = captchaRef.current.getValue();
+        console.log(token);
+        captchaRef.current.reset();
+        if(token === null || token === "" || token === undefined){
+            alert("Please verify that you are not a robot");
+            return;
+        }
+        await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=6LdM0eQkAAAAAAtY3BDfjaLlYeU_GX0mJx-s4lLC&response=${token}`).then((res) => {
+            console.log(res);
+            if(res.data.success === false || res.data.score < 0.5){
+                alert("Please verify that you are not a robot");
+                return;
+            }
+        }).catch((err) => {
+            console.log(err);
+        })
         signInWithPopup(auth, provider)
         .then((result) => {
             console.log(result);
@@ -22,6 +43,9 @@ const Login = () => {
             <div>
                 <button style={{"marginTop" : "200px"}} className='google-btn'
                 onClick={signin}>Sign In with Google</button>
+                <div style={{"marginTop" : "20px"}}>
+                <ReCaptcha sitekey='6LdM0eQkAAAAANZwcYgIcvO6LHEx--Y93d1YkPML' ref={captchaRef}/>
+                </div>
             </div>
         </div>
     );
